55ea024c064e2489591c82031cc9928533511c45
Patrick <kimanthip@gmail.com>
2021-09-06 17:43:04 +0300

    add setup and teardown methods

--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 from .models import Location
 # Create your tests here.
+
+class LocationTestCase(TestCase):
+    '''
+    Test class for the Location module
+    '''
+    def setUp(self):
+        '''
+        Method that creates an instance of the Location class before every test
+        '''
+        self.new_location = Location(name='Kitale')
+    
+    def tearDown(self):
+        Location.objects.all().delete()
